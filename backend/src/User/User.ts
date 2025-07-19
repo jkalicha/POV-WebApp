3400549b540bf25ef87bdb21bd6dca0b2e746857
jkalicha <166032860+jkalicha@users.noreply.github.com>
2025-07-19 14:20:49 -0300

    Formato basico y minimo de user

---
+++ backend/src/User/User.ts
@@ -0,0 +1,45 @@
+export class User {
+    private id: string;
+    private name: string;
+    private email: string;
+    private password: string;
+
+    constructor(id: string, name: string, email: string, password: string) {
+        // Validate inputs here --> check if email & password are valid
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public getPassword(): string {
+        return this.password;
+    }
+
+    public getId(): string {
+        return this.id;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public getEmail(): string {
+        return this.email;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    public setEmail(email: string): void {
+        // Validate email before setting
+        this.email = email;
+    }
+
+    public setPassword(password: string): void {
+        // Validate password before setting
+        this.password = password;
+    }
+}
