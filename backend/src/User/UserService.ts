008abac435101867f32a3e3fc006554a88d547c6
jkalicha <166032860+jkalicha@users.noreply.github.com>
2025-07-20 13:13:29 -0300

    Implementacion de IUserService y creacion de controlador

--- backend/src/User/UserService.ts
+++ backend/src/User/UserService.ts
@@ -1,3 +1,16 @@
-class UserService{
-    
+import { User } from "./User";
+import { UserRepository } from "./UserRepository";
+import { IUserService } from "../Shared/IUserService";
+
+export class UserService implements IUserService {
+    private _userRepository: UserRepository;
+
+    constructor(userRepository: UserRepository) {
+        this._userRepository = userRepository;
+    }
+
+    async createUser(name: string, email: string, password: string) {
+        const user = new User(name, email, password);
+        await this._userRepository.create(user);
+    }
 }
