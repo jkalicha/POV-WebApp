c8145833f981ebf5032509614f0588d50293535c
jkalicha <166032860+jkalicha@users.noreply.github.com>
2025-07-23 13:12:34 -0300

    Test happy path con Jest de UserController

---
+++ backend/tests/User/UserController.test.ts
@@ -0,0 +1,34 @@
+import { UserController } from "../../src/User/UserController";
+import { IUserService } from "../../src/Shared/IUserService";
+
+describe("UserController", () => {
+  let userController: UserController;
+  let mockUserService: jest.Mocked<IUserService>;
+
+  beforeEach(() => {
+    mockUserService = {
+      createUser: jest.fn(),
+    };
+
+    userController = new UserController(mockUserService);
+  });
+
+  describe("createUser", () => {
+    it("should delegate to userService", async () => {
+      const name = "Joaquin Kalichman";
+      const email = "joaquinkalichman@example.com";
+      const password = "Password123";
+
+      mockUserService.createUser.mockResolvedValue(undefined);
+
+      await userController.createUser(name, email, password);
+
+      expect(mockUserService.createUser).toHaveBeenCalledWith(
+        name,
+        email,
+        password
+      );
+      expect(mockUserService.createUser).toHaveBeenCalledTimes(1);
+    });
+  });
+});
