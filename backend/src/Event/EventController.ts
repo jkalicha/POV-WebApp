2c5e8c611554d063e0187d90f7e59bf784467ac4
jkalicha <166032860+jkalicha@users.noreply.github.com>
2025-07-24 15:44:33 -0300

    Implementacion event controller

---
+++ backend/src/Event/EventController.ts
@@ -0,0 +1,13 @@
+import { IEventService } from "../Shared/IEventService";
+
+export class EventController {
+  private _eventService: IEventService;
+
+  constructor(eventService: IEventService) {
+    this._eventService = eventService;
+  }
+
+  async createEvent(title: string, date: Date, location: string, ownerId: string): Promise<void> {
+    await this._eventService.createEvent(title, date, location, ownerId);
+  }
+}
