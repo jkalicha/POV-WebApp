ab212a1022d8db1d359110e2845d03705160cb86
jkalicha <166032860+jkalicha@users.noreply.github.com>
2025-09-03 13:50:16 -0300

    Ajuste de cerrar sesion

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.html',
   styleUrls: []
 })
 export class Home {
+  isLoggedIn = false;
 
+  constructor(private router: Router) {}
+
+  checkLogin(){
+    this.isLoggedIn = !!localStorage.getItem('token');
+  }
+
+  ngOnInit(){
+    this.checkLogin();
+    this.router.events.subscribe(() => {
+      this.checkLogin();
+    });
+  }
 }
