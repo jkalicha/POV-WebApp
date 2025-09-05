e6945eeeec2d7826e7731978ffeab8542dc7d88b
jkalicha <166032860+jkalicha@users.noreply.github.com>
2025-09-04 23:07:08 -0300

    Limpieza de login en el front

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(App, {
   providers: [
-    provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-    ...appConfig.providers,
-    HttpClientModule
+    ...appConfig.providers
   ]
 }).catch((err) => console.error(err));
